1a93f7357b34213dd97129ff5d2e60f0e3585f7c
Yongtak Jin <jinxx241@umn.edu>
2013-03-27 02:12:33 -0500

    update activitymain

--- NetworkBenchmark/gen/com/networkbenchmark/R.java
+++ NetworkBenchmark/gen/com/networkbenchmark/R.java
@@ -21,20 +21,42 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int backgroundword1=0x7f020001;
+        public static final int help=0x7f020002;
+        public static final int history=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int map=0x7f020006;
+        public static final int ping=0x7f020007;
+        public static final int result=0x7f020008;
+        public static final int save=0x7f020009;
+        public static final int speedtest2_1=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080001;
-        public static final int editText1=0x7f080000;
-        public static final int textView1=0x7f080002;
-        public static final int updatetv=0x7f080003;
+        public static final int action_settings=0x7f08000f;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f080007;
+        public static final int button5=0x7f080008;
+        public static final int button6=0x7f080009;
+        public static final int button7=0x7f08000b;
+        public static final int editText1=0x7f08000c;
+        public static final int imageView1=0x7f080001;
+        public static final int tableRow1=0x7f080002;
+        public static final int tableRow2=0x7f080006;
+        public static final int tableRow3=0x7f08000a;
+        public static final int tableRow4=0x7f080000;
+        public static final int textView1=0x7f08000d;
+        public static final int updatetv=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_view_perf_measure=0x7f030001;
         public static final int ping_view=0x7f030002;
         public static final int scroll_result=0x7f030003;
+        public static final int test3=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
